468cd71ec00cc28695a1c3c555a73fa675fcc03e
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-03-05 10:29:08 +0700

    menambahkan layout kalkulator

--- Modul2_1302200022/Modul2_1302200022/Form1.cs
+++ Modul2_1302200022/Modul2_1302200022/Form1.cs
@@ -16,5 +16,20 @@ namespace Modul2_1302200022
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
